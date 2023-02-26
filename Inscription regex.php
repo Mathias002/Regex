29d6d283bdaa65023d534a7081989d5c3a13a526
Mathias002 <mamousset@stpbb.org>
2023-02-26 14:32:27 +0100

    import

---
+++ Inscription regex.php
@@ -0,0 +1,78 @@
+<?php
+include 'header.php';
+?>
+
+<body>
+    <div class="container">
+        <div class="circle"></div>
+   
+            <div id="login">
+                <form method="POST" action="inscriptionValidate.php">                                  
+                        <!-- Register buttons -->
+                        <div class="text-center">
+                            <p>Veuillez vous inscrire :</p>
+                            <div>
+                                <label>Prénom : </label>
+                                <br>
+                                <input type="text" name="prenom" id="Prenom" placeholder="Votre prénom ..." onfocusout="verifPrenom()">
+                                <div id ="erreurPrenom"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Âge : </label>
+                                <br>
+                                <input type="number" name="age" id="Poids" placeholder="Votre âge ..." onfocusout="verifPoids()">
+                                <div id ="erreurPoids"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Sexe : </label>
+                                <br>
+                                <select name="sexe">
+                                    <option value="0">Garçon</option>
+                                    <option value="1">Fille</option>
+                                </select>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Mail : </label>
+                                <br>
+                                <input type="text" name="mail" id="Mail" placeholder="Votre mail ..." onfocusout="verifMail()">
+                                <div id ="erreurMail"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Confirmer votre mail : </label>
+                                <br>    
+                                <input type="text" name="confirmMail" id="CfMail" placeholder="Votre mail ..." onfocusout="verifCfMail()">
+                                <div id ="erreurCfMail"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Mot de passe : </label>
+                                <br>
+                                <input type="password" name="password" id="password" placeholder="Votre mot de passe ..." onfocusout="verifMdp()">
+                                <div id ="erreurPassword"></div>
+                            </div>
+                            <br>
+                            <div>
+                                <label>Confirmer votre mot de passe : </label>
+                                <br>
+                                <input type="password" name="confirmPassword" id="CfPassword" placeholder="Votre mot de passe ..." onfocusout="verifCfMdp()">
+                                <div id ="erreurCfPassword"></div>
+                            </div>
+                            <br>
+                            <div>
+                            <button type="submit" class="btn btn-primary btn-block mb-4">S'inscrire</button>
+                            <button type="reset" class="btn btn-primary btn-block mb-4">Annuler</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+<?php
+include "footer.php";
+?>
